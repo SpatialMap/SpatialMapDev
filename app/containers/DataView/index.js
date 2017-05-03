cdbcef7401357a8ceebd819693e79dfa1e5d7605
kohze <mlw.kohze@student.ru.nl>
2017-05-03 18:16:00 +0200

    added plot and dynamic linking

--- app/containers/DataView/index.js
+++ app/containers/DataView/index.js
@@ -11,12 +11,40 @@ import { FormattedMessage } from 'react-intl';
 import { createStructuredSelector } from 'reselect';
 import makeSelectDataView from './selectors';
 import messages from './messages';
+import ReactDOM from 'react-dom';
+import * as V from 'victory';
+import { VictoryScatter } from 'victory';
+import { VictoryChart } from 'victory';
+import { VictoryZoomContainer } from 'victory';
+import * as firebase from 'firebase';
+import './dataView.css';
 
 export class DataView extends React.Component {
   // eslint-disable-line react/prefer-stateless-function
-  render() {
-    const Query =  <h2> {this.props.params.uid}</h2>;
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataset: [],
+      test1: "123",
+    }
+  }
+
+  componentDidMount(){
+    var dataset = [];
+    firebase.database().ref('data/' + this.props.params.uid + '/pcaData').once("value", (snapshot) => {
+          let dataset = snapshot.val();
+          this.setState({
+            dataset
+          })
+    });
+
+  }
+  render() {
+    console.log(this.state.dataset);
+    const Query =  <h2> {this.props.params.uid}</h2>;
+    const data2 = this.state.dataset;
+    console.log({data2});
     return (
       <div>
         <Helmet
@@ -25,8 +53,44 @@ export class DataView extends React.Component {
             { name: 'description', content: 'Description of DataView' },
           ]}
         />
-        <FormattedMessage {...messages.header} />
-        {Query}
+        <div className="mainPlot">
+        <VictoryChart className="mainPlot" standalone={false} containerComponent={<VictoryZoomContainer  style={{width: 800, height: 800}} standalone={true}  />}
+        >
+        <VictoryScatter
+        standalone={false}
+        data={data2}
+        x="PCA1"
+        y="PCA2"
+        events={[
+  {
+    target: "data",
+    eventHandlers: {
+      onClick: () => {
+        return [
+          {
+            mutation: (props) => {
+              return {
+                style: Object.assign({}, props.style, {fill: "orange"})
+              };
+            }
+          }, {
+            target: "labels",
+            mutation: () => {
+              return {text: "works"};
+            },
+            callback: () => {
+              console.log("I happen after setState");
+            }
+          }
+        ];
+      }
+    }
+  }
+ ]}
+      />
+      </VictoryChart>
+
+      </div>
       </div>
     );
   }
