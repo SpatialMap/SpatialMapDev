433b6f3ce436974da134a0dee4d0c852ee77345f
kohze <mlw.kohze@student.ru.nl>
2017-05-08 20:11:16 +0200

    added DataView command

--- app/containers/DataView/dataPoints.js
+++ app/containers/DataView/dataPoints.js
@@ -3,20 +3,35 @@ import React from 'react';
 const renderCircles = (props) => {
   return (coords, index) => {
     var indexRad = index == 15 ? 10 : 2;
+    // conditional statements inside variables to select certain elements
     var colorVar = coords.Markers == "unknown" ? "rgba(100,100,100,0.1)" : coords.Colors ;
+    var textVar = coords.Markers == "unknown" ? "" : coords.Markers ;
     var strokeVar = coords.Markers == "unknown" ? "rgba(100,100,100,0)" : "none" ;
+    //x,y coordinates
+    var xPOS = props.xScale(coords.PCA1);
+    var yPOS = props.yScale(coords.PCA2);
     const circleProps = {
-      cx: props.xScale(coords.PCA1),
-      cy: props.yScale(coords.PCA2),
-      r: 3,
+      cx: xPOS,
+      cy: yPOS,
       fill: colorVar,
       stroke: strokeVar,
       key: index
+      //radius defined in css
+      //hoover defined in css
     };
-    return <circle {...circleProps} />;
+
+    const textProps = {
+      x: xPOS,
+      y: yPOS,
+      //index + number to generate a unique key
+      key: index+0.1
+      //textsize defined in css
+    };
+
+    return <g className="circleText" key={index+0.2}> <circle {...circleProps} /> <text {...textProps}> {textVar} </text> </g>;
   };
 };
 
 export default (props) => {
-  return <g onMouseEnter={console.log("test")}>{ props.data.map(renderCircles(props)) }</g>
+  return <g >{ props.data.map(renderCircles(props)) }</g>
 }
--- app/containers/DataView/index.js
+++ app/containers/DataView/index.js
@@ -13,15 +13,18 @@ import { createStructuredSelector } from 'reselect';
 import makeSelectDataView from './selectors';
 import messages from './messages';
 import ReactDOM from 'react-dom';
-// import * as V from 'victory';
-// import { VictoryScatter } from 'victory';
-// import { VictoryChart } from 'victory';
-// import { VictoryZoomContainer } from 'victory';
 import * as firebase from 'firebase';
 import './dataView.css';
 import ScatterPlot from './scatter.js';
-import {ReactSVGPanZoom} from 'react-svg-pan-zoom';
+import { ReactSVGPanZoom } from 'react-svg-pan-zoom';
 import keydown, { Keys } from 'react-keydown';
+import { PrimaryButton, DefaultButton } from 'office-ui-fabric-react/lib/Button';
+import { Spinner, SpinnerSize } from 'office-ui-fabric-react/lib/Spinner';
+import { Checkbox } from 'office-ui-fabric-react/lib/Checkbox';
+import { Slider } from 'office-ui-fabric-react/lib/Slider';
+import { Toggle } from 'office-ui-fabric-react/lib/Toggle';
+import { Label } from 'office-ui-fabric-react/lib/Label';
+import { CompoundButton, IButtonProps } from 'office-ui-fabric-react/lib/Button';
 var Dimensions = require('react-dimensions');
 
 const styles = {
@@ -39,12 +42,16 @@ export class DataView extends React.Component {
       test1: "123",
       data: [],
       data2: [],
-    }
+      loading: true,
+      value: 4,
+    };
   }
 
+  //svg zoom initialization
   componentDidMount() {
     this.Viewer.fitToViewer();
   }
+
   //loading scatterplot data from firebase
   componentDidMount(){
     var data = [];
@@ -68,6 +75,34 @@ export class DataView extends React.Component {
 
     const Query =  <h2> {this.props.params.uid}</h2>;
     const data = this.state.data
+    const d3Plot =  <div className="scatterContainer" >
+                      <ReactSVGPanZoom
+                        style={{}}
+                        width={1800} height={500} ref={Viewer => this.Viewer = Viewer}
+                        onClick={event => console.log('click', event.x, event.y, event.originalEvent)}
+                        onMouseMove={event => console.log('move', event.x, event.y)}
+                        SVGBackground="white"
+                        background="white"
+                        detectWheel={true}
+                        // detectAutoPan={false}
+                        miniatureWidth={50}
+                        toolbarPosition={"none"}
+                        tool={"auto"}
+                        detectAutoPan={false}
+                        >
+                        <svg width={1800} height={500}>
+                          <ScatterPlot {...this.state} {...styles}  />
+                        </svg>
+                    </ReactSVGPanZoom>
+                  </div>
+
+    const d3Loader =  <div>
+                        <Spinner size={SpinnerSize.large} />
+                      </div>;
+
+    const d3Container = this.loading == true ? d3Loader : d3Plot;
+
+
     // const data = this.state.data;
     return (
       <div>
@@ -77,32 +112,71 @@ export class DataView extends React.Component {
             { name: 'description', content: 'Description of DataView' },
           ]}
         />
-        <div className="info">
-        <p> header info placeholder </p>
-        </div>
-        <div className="scatterContainer" >
-        <ReactSVGPanZoom
-          style={{}}
-          width={1800} height={500} ref={Viewer => this.Viewer = Viewer}
-          onClick={event => console.log('click', event.x, event.y, event.originalEvent)}
-          onMouseMove={event => console.log('move', event.x, event.y)}
-          SVGBackground="white"
-          background="white"
-          detectWheel={true}
-          // detectAutoPan={false}
-          miniatureWidth={50}
-          toolbarPosition={"none"}
-          tool={"auto"}
-          >
-          <svg width={1000} height={500}>
-            <ScatterPlot {...this.state} {...styles}  />
-          </svg>
-        </ReactSVGPanZoom>
 
-        <div className="table">
-        <p> table placeholder </p>
+        <div className="configBar">
+          <div className="buttons">
+            <DefaultButton
+                description='Opens the Sample Dialog'
+                text='Citation'
+            />
+          </div>
+          <div className="buttons">
+            <DefaultButton
+                description='Opens the Sample Dialog'
+                text='Download'
+            />
+          </div>
+          <div className="buttons">
+            <DefaultButton
+                description='Opens the Sample Dialog'
+                text='Meta Data'
+            />
+         </div>
+         <div className="slider">
+            <Slider
+              label='Scatter Point Size'
+              min={ 1 }
+              max={ 10 }
+              step={ 1 }
+              defaultValue={ 4 }
+              onChange={ value => this.setState({ value }) }
+              showValue={ false }
+            />
+         </div>
+         <div className="toggle">
+             <Toggle
+              defaultChecked={ true }
+              label='Display Labels'
+              onAriaLabel='This toggle is checked. Press to uncheck.'
+              offAriaLabel='This toggle is unchecked. Press to check.'
+              onText='On'
+              offText='Off' />
         </div>
-        </div>
+        <div className="toggle">
+            <Toggle
+             defaultChecked={ true }
+             label='Display Unknown'
+             onAriaLabel='This toggle is checked. Press to uncheck.'
+             offAriaLabel='This toggle is unchecked. Press to check.'
+             onText='On'
+             offText='Off' />
+       </div>
+       <div className="buttonsRight">
+           <DefaultButton
+               description='Opens the Sample Dialog'
+               text='Comments'
+           />
+      </div>
+      </div>
+      <div className="info">
+        <p> header info placeholder </p>
+      </div>
+      <div className="mainPlot">
+          {d3Container}
+      </div>
+      <div className="table">
+          <p> table placeholder </p>
+      </div>
       </div>
       )
   }
