fb7c73c56b782a349ca8977575acbfe551f5e814
Kohze <mlw.kohze@student.ru.nl>
2017-08-04 23:56:42 +0200

    added description to home page

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -29,9 +29,8 @@ export default class HomePage extends React.PureComponent { // eslint-disable-li
              </div>
              <div className="col-sm-6 textCard">
              <h1 className="headText"> What if </h1>
-             <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-             tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero
-               takimata sanctus est Lorem.</p>
+             <p>there was a platform to collect and compare spatial proteomics datasets.
+               A database that offers a user friendly interface that is still accessible by programming languages such as R.</p>
              <button className="headButton"> Get Started </button>
            </div>
           </div>
@@ -86,12 +85,8 @@ export default class HomePage extends React.PureComponent { // eslint-disable-li
           <div className="row container" style={{margin:"auto", marginTop: 30}}>
             <div className="col-sm-6 bottomCard">
             <h2 className="bottomText"> Technology </h2>
-            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-            tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero
-              takimata sanctus est Lorem.</p>
-              <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-              tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero
-                takimata sanctus est Lorem.</p>
+            <p>The SpatialMaps platform is based on Google Firebase as secure cloud database and react.js. </p>
+              <p>Combining the cloud database with pRolocdata biocoductor package, we offer the opportunity access datasets both via R scripts as well as via the user friendly SpatialMaps web platform.</p>
             </div>
             <div className="col-sm-6">
               <img className="headImage" src="http://frapbot.kohze.com/other/welcome_first.png"/>
